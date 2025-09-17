60b6cce0a52348c00827757be75a0aa5a1f621c5
Pedro Silva <43070901+phsilva1503@users.noreply.github.com>
2025-09-17 00:18:12 -0300

    Delete to-do directory

--- to-do/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-
-class CadastroForm(FlaskForm):
-    usuario = StringField('Username:')
-    email = StringField('E-mail:')
-    senha1 = PasswordField('Senha:')
-    senha2 = PasswordField('Confirmação de Senha:')
-    submit = SubmitField('Cadastrar')
